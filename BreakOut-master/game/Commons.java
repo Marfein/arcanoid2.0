4771d43c78d8d1a7eac8cc9a57eb491f6931570e
Marfein <mitskevich10@mail.ru>
2021-06-06 22:32:31 +0900

    1

---
+++ BreakOut-master/game/Commons.java
@@ -0,0 +1,17 @@
+package game;
+
+public interface Commons {
+
+	int WIDTH = 300;
+	int HEIGHT = 400;
+	int BOTTOM_EDGE = 390;
+	int N_OF_BRICKS = 30;
+	int N_OF_WIDTH_BRIKS = 5;
+	int N_OF_HEIGHT_BRIKS = 6;
+	int INIT_PADDLE_X = 200;
+	int INIT_PADDLE_Y = 360;
+	int INIT_BALL_X = 230;
+	int INIT_BALL_Y = 300;
+	int PERIOD = 1;
+	
+}
